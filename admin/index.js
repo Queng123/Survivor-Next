87df2f8ee4d12e3b576fd01db758bedc1c6a7802
queng <quentin.brejoin@gmail.com>
2023-09-11 15:25:20 +0200

    fix(Core): update index.js with good filepath

--- admin/index.js
+++ admin/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
-const test = require("./src/test");
 
-app.use("/src/test", test);
+app.get("/test", async (req, res) => {
+    res.send("Hello World!");
+});
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
