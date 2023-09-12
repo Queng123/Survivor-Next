41990db2d9f8b6e2a56b4403a2c3aed400666f8f
quentin <Quentin.brejoin@gmail.com>
2023-09-12 18:22:34 +0200

    refactor(dashboard): update db (#73)
    Co-authored-by: Miouzora <91665686+Miou-zora@users.noreply.github.com>

--- admin/queries/api.queries.js
+++ admin/queries/api.queries.js
@@ -2,7 +2,7 @@ const createModel = require('../database/models/dashboard.model');
 
 exports.getJson = (uuid) => {
     const Dashboard = createModel(uuid);
-    return Dashboard.findOne({ _id: uuid }).select('-logo').exec();
+    return Dashboard.findOne({ _id: uuid }).select('-logo').select('-_timestamp').select('-_id').select('-__v').exec();
   }
 
 exports.getTimestamp = (uuid) => {
