71ac4106f7d374cf19021a5e26b12c64582d6781
miouzora <alexandre.franquet@epitech.eu>
2023-09-12 17:13:56 +0200

    refactor(LanguageButton): add i18n in useEffect dependencies

--- trombi/src/components/LanguageButton.tsx
+++ trombi/src/components/LanguageButton.tsx
@@ -1,33 +1,33 @@
-import React from "react";
-import { View, Text, Button, StyleSheet } from "react-native";
+import React from 'react';
+import {View, StyleSheet} from 'react-native';
 import DropDownPicker from 'react-native-dropdown-picker';
 import '../../locales/index';
-import { useTranslation } from 'react-i18next';
+import {useTranslation} from 'react-i18next';
 
 function LanguageButton(): JSX.Element {
-  const {t, i18n} = useTranslation();
+  const {i18n} = useTranslation();
 
   const [open, setOpen] = React.useState(false);
   const [value, setValue] = React.useState(i18n.language);
   const [items, setItems] = React.useState([
     {label: 'Francais', value: 'fr'},
-    {label: 'English', value: 'en'}
+    {label: 'English', value: 'en'},
   ]);
 
   React.useEffect(() => {
     i18n.changeLanguage(value);
-  }, [value]);
+  }, [value, i18n]);
 
   return (
     <View style={styles.container}>
-        <DropDownPicker
-          open={open}
-          value={value}
-          items={items}
-          setOpen={setOpen}
-          setValue={setValue}
-          setItems={setItems}
-        />
+      <DropDownPicker
+        open={open}
+        value={value}
+        items={items}
+        setOpen={setOpen}
+        setValue={setValue}
+        setItems={setItems}
+      />
     </View>
   );
 }
