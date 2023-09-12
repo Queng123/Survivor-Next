3b7c498f9aa317f65863d7b229674737cafbb42e
queng <quentin.brejoin@gmail.com>
2023-09-12 13:45:47 +0200

    feat(API): add route to get timestamp value

--- admin/queries/api.queries.js
+++ admin/queries/api.queries.js
@@ -4,3 +4,16 @@ exports.getJson = (uuid) => {
     const Dashboard = createModel(uuid);
     return Dashboard.findOne({ _id: uuid }).exec();
   }
+
+exports.getTimestamp = (uuid) => {
+    const Dashboard = createModel(uuid);
+    return Dashboard.findOne({ _id: uuid }, { _timestamp: 1 })
+    .exec()
+    .then((document) => {
+        if (document) {
+          return document._timestamp;
+        } else {
+          return null;
+        }
+      });
+  }
--- admin/routes/api.routes.js
+++ admin/routes/api.routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
-const { custom } = require('../controllers/api.controller');
+const { custom, update } = require('../controllers/api.controller');
 
 router.get('/company/custom/:uuid', custom);
+router.get('/company/update/:uuid', update);
 
 module.exports = router;
