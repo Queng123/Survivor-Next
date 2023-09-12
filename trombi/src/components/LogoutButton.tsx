bcd5a1bd955d5c9e117c713eacc27654f42eb29e
miouzora <alexandre.franquet@epitech.eu>
2023-09-12 18:40:19 +0200

    feat(logoutButton): create a logout button

---
+++ trombi/src/components/LogoutButton.tsx
@@ -0,0 +1,33 @@
+import React from "react";
+import { View, Button, StyleSheet } from "react-native";
+
+import '../../locales/index';
+import { useTranslation } from 'react-i18next';
+
+import { useNavigation } from '@react-navigation/native';
+
+
+function LogoutButton()
+{
+    const {t} = useTranslation();
+    const navigation = useNavigation();
+
+    const deconnexion = () => {
+        navigation.navigate("Login");
+        // TODO: remove user token
+    };
+
+    return (
+        <View style={styles.container}>
+            <Button title={t("settings.logout")} onPress={deconnexion}/>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        padding: 20,
+    },
+});
+
+export default LogoutButton;
