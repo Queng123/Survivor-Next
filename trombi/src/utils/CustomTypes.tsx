4af5f87056a8c531f735183e6c3cd71d9f41919b
Lucas <70372762+ripel2@users.noreply.github.com>
2023-09-12 22:56:53 +0200

    feat(custom): add custom api state fetching

---
+++ trombi/src/utils/CustomTypes.tsx
@@ -0,0 +1,37 @@
+export type CustomData = {
+    "company-uuid": string,
+    "group-token": string,
+    "company-api-url": string,
+    "extern-api-token": {
+        "weather": string,
+        "stock": string,
+        "gpt3": string
+    },
+    "custom": {
+        "background-1": string,
+        "background-1-dark": string,
+        "background-2": string,
+        "background-2-dark": string,
+        "background-3": string,
+        "background-3-dark": string,
+        "background-4": string,
+        "background-4-dark": string,
+        "button-primary": string,
+        "button-primary-dark": string,
+        "button-secondary": string,
+        "button-secondary-dark": string,
+        "button-primary-foreground": string,
+        "button-primary-foreground-dark": string,
+        "button-secondary-foreground": string,
+        "button-secondary-foreground-dark": string,
+        "title-primary": string,
+        "title-primary-dark": string,
+        "title-secondary": string,
+        "title-secondary-dark": string,
+        "text-primary": string,
+        "text-primary-dark": string,
+        "text-secondary": string,
+        "text-secondary-dark": string,
+        "default-language": string
+    }
+}
--- trombi/src/utils/GlobalStore.tsx
+++ trombi/src/utils/GlobalStore.tsx
@@ -1,8 +1,10 @@
 import {configureStore} from '@reduxjs/toolkit';
 import widgetReducer from './WidgetSlice';
+import customReducer from './CustomSlice';
 
 export const store = configureStore({
   reducer: {
     widget: widgetReducer,
+    custom: customReducer,
   },
 });
