b0d8af940c773c1c9e86263c2d141e7558300e0d
Axel <91660228+FoxaxeWasTaken@users.noreply.github.com>
2023-09-12 16:19:51 +0200

    feat: add the navigation and the navbar (#65)
    * feat: add the base architecture and the navbar
    
    * feat: add dependencies for the navbar and navigation
    
    * feat: add a gitkeep to add the utils src folder

---
+++ trombi/src/pages/WidgetSelector.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const WidgetSelector = () => {
+  return (
+    <View>
+      <Text>Widget Selector</Text>
+    </View>
+  );
+};
+export default WidgetSelector;
---
+++ trombi/src/pages/Widgets.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Widgets = () => {
+  return (
+    <View>
+      <Text>Widgets</Text>
+    </View>
+  );
+};
+export default Widgets;
