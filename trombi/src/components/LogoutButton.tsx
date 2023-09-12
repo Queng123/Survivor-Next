f9850ed347a268f7c93e3f4b21348dba4ef70f59
miouzora <alexandre.franquet@epitech.eu>
2023-09-12 18:41:25 +0200

    refactor(eslint): make code eslint compliant

--- trombi/src/components/LogoutButton.tsx
+++ trombi/src/components/LogoutButton.tsx
@@ -1,33 +1,31 @@
-import React from "react";
-import { View, Button, StyleSheet } from "react-native";
+import React from 'react';
+import {View, Button, StyleSheet} from 'react-native';
 
 import '../../locales/index';
-import { useTranslation } from 'react-i18next';
+import {useTranslation} from 'react-i18next';
 
-import { useNavigation } from '@react-navigation/native';
+import {useNavigation} from '@react-navigation/native';
 
+function LogoutButton() {
+  const {t} = useTranslation();
+  const navigation = useNavigation();
 
-function LogoutButton()
-{
-    const {t} = useTranslation();
-    const navigation = useNavigation();
+  const deconnexion = () => {
+    navigation.navigate('Login');
+    // TODO: remove user token
+  };
 
-    const deconnexion = () => {
-        navigation.navigate("Login");
-        // TODO: remove user token
-    };
-
-    return (
-        <View style={styles.container}>
-            <Button title={t("settings.logout")} onPress={deconnexion}/>
-        </View>
-    )
+  return (
+    <View style={styles.container}>
+      <Button title={t('settings.logout')} onPress={deconnexion} />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        padding: 20,
-    },
+  container: {
+    padding: 20,
+  },
 });
 
 export default LogoutButton;
