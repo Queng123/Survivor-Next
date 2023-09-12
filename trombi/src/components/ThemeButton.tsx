2e80a47766c2380cce5a4b54d257b70c5e23aebb
miouzora <alexandre.franquet@epitech.eu>
2023-09-12 17:55:41 +0200

    feat(ThemeButton): add theme button

---
+++ trombi/src/components/ThemeButton.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { View, Text, Switch, StyleSheet } from 'react-native';
+
+const ThemeButton = () => {
+    const [isEnabled, setIsEnabled] = React.useState(false);
+
+    return (
+        <View style={styles.container}>
+            <Switch
+                trackColor={{ false: "#ccc", true: "#333" }}
+                thumbColor={isEnabled ? "#aaa" : "#f4f3f4"}
+                ios_backgroundColor={isEnabled ? "#333" : "#ccc"}
+                onValueChange={setIsEnabled}
+                value={isEnabled}
+                style={styles.switch}
+            />
+            <Text style={styles.text}>{isEnabled ? "Mode Sombre" : "Mode Clair"}</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        padding: 30,
+    },
+    text: {
+        color: "#333",
+        marginLeft: 30,
+        fontSize: 24,
+        fontWeight: 'bold',
+    },
+    switch: {
+        transform: [{ scaleX: 1.5 }, { scaleY: 1.5 }],
+    },
+});
+
+export default ThemeButton;
