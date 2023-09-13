68fcebf70d6b72f8668da8df7821ff4d431449d3
Lucas <70372762+ripel2@users.noreply.github.com>
2023-09-13 09:27:09 +0200

    refactor: fix eslint errors

--- trombi/src/utils/CustomFunctions.tsx
+++ trombi/src/utils/CustomFunctions.tsx
@@ -1,106 +1,113 @@
-import { store } from './GlobalStore';
-import { CustomData } from './CustomTypes';
+import {store} from './GlobalStore';
+import {CustomData} from './CustomTypes';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import { ADMIN_API_URL, COMPANY_UUID } from "@env"
+import {ADMIN_API_URL, COMPANY_UUID} from '@env';
 
-export const fetchCustomDataFromLocalStorage = async (): Promise<CustomData> => {
+export const fetchCustomDataFromLocalStorage =
+  async (): Promise<CustomData> => {
     const promise = new Promise<CustomData>((resolve, reject) => {
-        AsyncStorage.getItem('customData').then(value => {
+      AsyncStorage.getItem('customData').then(value => {
         if (value === null) {
-            reject('No custom data found in local storage');
+          reject('No custom data found in local storage');
         } else {
-            const customData = JSON.parse(value);
-            resolve(customData);
+          const customData = JSON.parse(value);
+          resolve(customData);
         }
-        });
+      });
     });
     return promise;
-}
+  };
 
 export const setCustomDataInLocalStorage = async (customData: CustomData) => {
-    AsyncStorage.setItem('customData', JSON.stringify(customData));
-}
+  AsyncStorage.setItem('customData', JSON.stringify(customData));
+};
 
 export const fetchLatestUpdateFromApi = async (): Promise<string> => {
-    const promise = new Promise<string>((resolve, reject) => {
-        const url = `${ADMIN_API_URL}/company/update/${COMPANY_UUID}`;
-        fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            resolve(data);
-        })
-        .catch(error => {
-            reject(error);
-        });
-    });
-    return promise;
-}
+  const promise = new Promise<string>((resolve, reject) => {
+    const url = `${ADMIN_API_URL}/company/update/${COMPANY_UUID}`;
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        resolve(data);
+      })
+      .catch(error => {
+        reject(error);
+      });
+  });
+  return promise;
+};
 
 export const setLatestUpdateInLocalStorage = async (latestUpdate: string) => {
-    AsyncStorage.setItem('customLatestUpdate', latestUpdate);
-}
+  AsyncStorage.setItem('customLatestUpdate', latestUpdate);
+};
 
 export const fetchLatestUpdateFromLocalStorage = async (): Promise<string> => {
-    const promise = new Promise<string>((resolve, _) => {
-        AsyncStorage.getItem('customLatestUpdate').then(value => {
-        if (value === null) {
-            resolve('');
-        } else {
-            resolve(value);
-        }
-        });
+  const promise = new Promise<string>((resolve, _) => {
+    AsyncStorage.getItem('customLatestUpdate').then(value => {
+      if (value === null) {
+        resolve('');
+      } else {
+        resolve(value);
+      }
     });
-    return promise;
-}
+  });
+  return promise;
+};
 
 export const fetchCustomDataFromApi = async (): Promise<CustomData> => {
-    const promise = new Promise<CustomData>((resolve, reject) => {
-        const url = `${ADMIN_API_URL}/company/custom/${COMPANY_UUID}`;
-        fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            resolve(data);
-        })
-        .catch(error => {
-            reject(error);
-        });
-    });
-    return promise;
-}
+  const promise = new Promise<CustomData>((resolve, reject) => {
+    const url = `${ADMIN_API_URL}/company/custom/${COMPANY_UUID}`;
+    fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        resolve(data);
+      })
+      .catch(error => {
+        reject(error);
+      });
+  });
+  return promise;
+};
 
 export const setCustomState = (customData: CustomData) => {
-    store.dispatch({type: 'custom/setCustomState', payload: customData});
-}
+  store.dispatch({type: 'custom/setCustomState', payload: customData});
+};
 
 export const getCustomState = (): CustomData => {
-    return store.getState().custom.customState;
-}
+  return store.getState().custom.customState;
+};
 
 export const appInitCustomData = async () => {
-    const fetchCatch = (error1: Error | string) => {
-        console.debug("API Fetch reason", error1);
-        fetchCustomDataFromApi().then(customData => {
-            setCustomState(customData);
-            setCustomDataInLocalStorage(customData);
-        }).catch(error => {
-            console.error("Current error", error);
-        });
-    }
-
-    fetchCustomDataFromLocalStorage().then(customData => {
+  const fetchCatch = (error1: Error | string) => {
+    console.debug('API Fetch reason', error1);
+    fetchCustomDataFromApi()
+      .then(customData => {
         setCustomState(customData);
-        fetchLatestUpdateFromLocalStorage().then(latestUpdate => {
-            console.debug("Fetch from local storage latest update", latestUpdate);
-            fetchLatestUpdateFromApi().then(latestUpdateApi => {
-                if (latestUpdate !== latestUpdateApi) {
-                    fetchCatch("latestUpdate !== latestUpdateApi");
-                }
-                setLatestUpdateInLocalStorage(latestUpdateApi);
-            }).catch(error2 => {
-                console.error("Fail to fetch latest update from api", error2);
-            });
-        })
-    }).catch(error1 => {
-        fetchCatch(error1);
+        setCustomDataInLocalStorage(customData);
+      })
+      .catch(error => {
+        console.error('Current error', error);
+      });
+  };
+
+  fetchCustomDataFromLocalStorage()
+    .then(customData => {
+      setCustomState(customData);
+      fetchLatestUpdateFromLocalStorage().then(latestUpdate => {
+        console.debug('Fetch from local storage latest update', latestUpdate);
+        fetchLatestUpdateFromApi()
+          .then(latestUpdateApi => {
+            if (latestUpdate !== latestUpdateApi) {
+              fetchCatch('latestUpdate !== latestUpdateApi');
+            }
+            setLatestUpdateInLocalStorage(latestUpdateApi);
+          })
+          .catch(error2 => {
+            console.error('Fail to fetch latest update from api', error2);
+          });
+      });
+    })
+    .catch(error1 => {
+      fetchCatch(error1);
     });
-}
+};
