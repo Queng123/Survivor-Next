0bc2796592a92e74ec9192ef6a3435ddfd6d6d8a
Lucas <70372762+ripel2@users.noreply.github.com>
2023-09-13 14:45:15 +0200

    feat(login) get all the user information (#98)
    * refactor: now truly get photo from API
    
    * feat: add /me request to get current user infos
    
    * feat: add UserInfo component and page
    
    * feat(profile): use local stored state tokens to fetch profile
    
    * refactor: fix eslint errors
    
    ---------
    
    Co-authored-by: Hugoguiheneuf <hugo.guiheneuf9@gmail.com>
    Co-authored-by: Miouzora <91665686+Miou-zora@users.noreply.github.com>

---
+++ trombi/src/components/UserInfo.tsx
@@ -0,0 +1,89 @@
+import React, {useEffect, useState} from 'react';
+import {StyleSheet, View, Linking} from 'react-native';
+import {useRoute, useNavigation} from '@react-navigation/native';
+
+import {ScrollView} from 'react-native-gesture-handler';
+import {ProfileInfo} from './ProfileInfo';
+import {CustomButton} from './CustomButton';
+
+export const getUserInfos = async (id: number) => {
+  try {
+    const response = await fetch(`https://masurao.fr/api/employees/${id}`, {
+      method: 'GET',
+      headers: {
+        accept: 'application/json',
+        'X-Group-Authorization': '',
+        Authorization: '',
+      },
+    });
+
+    if (!response.ok) {
+      console.error(`Request failed with status ${response.status}`);
+    }
+
+    const employeeInformations = await response.json();
+    return employeeInformations;
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const UserInfo = () => {
+  const navigation = useNavigation();
+  const route = useRoute();
+  const {id} = route.params;
+  const [userInfo, setUserInfo] = useState<any>(null);
+
+  useEffect(() => {
+    const fetchUserInfo = async () => {
+      try {
+        const userInformation = await getUserInfos(id);
+        setUserInfo(userInformation);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    fetchUserInfo();
+  }, [id]);
+
+  return (
+    <ScrollView>
+      <View style={styles.container}>
+        <ProfileInfo
+          id={userInfo.id}
+          name={userInfo.name}
+          post={userInfo.work}
+          email={userInfo.email}
+          birthday={userInfo.birthday}
+          gender={userInfo.gender}
+        />
+      </View>
+      <CustomButton
+        title="Chat"
+        iconName="chatbubble-outline"
+        onPress={() => navigation.navigate('Chat')}
+      />
+      <CustomButton
+        title="Email"
+        iconName="mail-outline"
+        onPress={() => Linking.openURL('mailto:oliver.lewis@masurao.jp')}
+      />
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+  },
+  settingsButton: {
+    position: 'absolute',
+    top: 30,
+    right: 30,
+  },
+});
+
+export default UserInfo;
