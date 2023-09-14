63dc56578f7c47f7ad0995054e5e33a89133f14f
queng <quentin.brejoin@gmail.com>
2023-09-14 16:50:16 +0200

    feat(API): add route to get user chat token

--- admin/routes/api.routes.js
+++ admin/routes/api.routes.js
@@ -1,9 +1,21 @@
 const router = require('express').Router();
 const { custom, update, logo, all } = require('../controllers/api.controller');
+const StreamChat = require('stream-chat').StreamChat;
+const chat_key = process.env['CHAT_KEY'];
+const chat_secret = process.env['CHAT_SECRET'];
+
+const serverClient = StreamChat.getInstance(chat_key, chat_secret);
 
 router.get('/company/custom/:uuid', custom);
 router.get('/company/all/:uuid', all);
 router.get('/company/update/:uuid', update);
 router.get('/company/logo/:uuid', logo);
+router.get('/token/:user',
+    async (req, res) => {
+        const { user } = req.params;
+        const token = serverClient.createToken(user);
+        res.status(200).send(token);
+    }
+);
 
 module.exports = router;
