5ad35cfc86dacf127913e0071b3e2be56f4dfdb3
queng <quentin.brejoin@gmail.com>
2023-09-14 17:47:34 +0200

    feat(Api): add create channel route

--- admin/routes/api.routes.js
+++ admin/routes/api.routes.js
@@ -10,11 +10,22 @@ router.get('/company/custom/:uuid', custom);
 router.get('/company/all/:uuid', all);
 router.get('/company/update/:uuid', update);
 router.get('/company/logo/:uuid', logo);
-router.get('/token/:user',
+router.get('/chat/token/:user',
     async (req, res) => {
         const { user } = req.params;
         const token = serverClient.createToken(user);
         res.status(200).send(token);
+    }
+);
+router.get('/chat/channel/:user',
+    async (req, res) => {
+        const { user } = req.params;
+        const channel = serverClient.channel('messaging', 'travel', {
+          name: 'test mon chef',
+          created_by_id: user,
+        });
+        await channel.create();
+        res.status(200);
     }
 );
 
