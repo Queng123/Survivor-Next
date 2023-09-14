67e92bd9c53836a2ddb968c4df0eb8d2a52a9b3c
queng <quentin.brejoin@gmail.com>
2023-09-14 18:52:28 +0200

    refactor(Api): add user creation

--- admin/routes/api.routes.js
+++ admin/routes/api.routes.js
@@ -3,10 +3,8 @@ const { custom, update, logo, all } = require('../controllers/api.controller');
 const StreamChat = require('stream-chat').StreamChat;
 const chat_key = process.env['CHAT_KEY'];
 const chat_secret = process.env['CHAT_SECRET'];
-const stream = require('getstream');
 
 const serverClient = StreamChat.getInstance(chat_key, chat_secret);
-const client = stream.connect(chat_key, chat_secret);
 
 router.get('/company/custom/:uuid', custom);
 router.get('/company/all/:uuid', all);
