e37e2cc04fd503772d8d8cd9f61a925b1446b2c6
miouzora <alexandre.franquet@epitech.eu>
2023-09-14 18:58:35 +0200

    feat(language): call api if needed

--- trombi/src/Root.tsx
+++ trombi/src/Root.tsx
@@ -13,6 +13,7 @@ import {ThemeProvider} from './utils/ThemeContext';
 import {useNavigation, CommonActions} from '@react-navigation/native';
 import {fetchLanguageFromLocalStorage} from './utils/LanguageFunctions';
 import {useTranslation} from 'react-i18next';
+import { getCustomState } from './utils/CustomFunctions';
 
 const Stack = createStackNavigator();
 
@@ -56,7 +57,7 @@ function Root() {
         i18n.changeLanguage(language.language);
       })
       .catch(() => {
-        i18n.changeLanguage('en');
+        i18n.changeLanguage(getCustomState().custom['default-language'] === 'french' ? 'fr' : 'en');
       });
   }, [navigation, i18n]);
 
