2ae4010450a3a03c8dcea0f2eaece6f97737b543
Lucas <70372762+ripel2@users.noreply.github.com>
2023-09-14 18:36:58 +0200

    feat(widgets): add youtube widget (#134)
    * feat(youtube): add working oauth google connection
    
    * feat(youtube): add working youtube widget
    
    * refactor: fix eslint errors
    
    * feat(youtube): add logout

--- trombi/App.tsx
+++ trombi/App.tsx
@@ -4,10 +4,23 @@ import {NavigationContainer} from '@react-navigation/native';
 import {Provider} from 'react-redux';
 import {store} from './src/utils/GlobalStore';
 import {appInitCustomData} from './src/utils/CustomFunctions';
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
+import {GOOGLE_CLIENT_ID} from '@env';
 
 function App(): JSX.Element {
   appInitCustomData();
-
+  GoogleSignin.configure({
+    webClientId: `${GOOGLE_CLIENT_ID}`,
+    scopes: [
+      'email',
+      'profile',
+      'https://www.googleapis.com/auth/youtube',
+      'https://www.googleapis.com/auth/youtube.readonly',
+      'https://www.googleapis.com/auth/youtube.force-ssl',
+    ],
+    offlineAccess: true,
+    forceCodeForRefreshToken: false,
+  });
   return (
     <Provider store={store}>
       <NavigationContainer>
