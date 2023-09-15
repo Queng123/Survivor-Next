0214aa9c8b216e4685b88935888128eb537adb86
Eliot Baratte <eliot.baratte@epitech.eu>
2023-09-15 08:26:01 +0200

    feat(trombi-page): make the page customizable via the database

--- trombi/src/components/ProfileCardComponent.tsx
+++ trombi/src/components/ProfileCardComponent.tsx
@@ -3,6 +3,7 @@ import {View, Text, TouchableHighlight, StyleSheet} from 'react-native';
 import FastImage from 'react-native-fast-image';
 import {getTokens} from '../utils/TokenFunctions';
 import {getCustomState} from '../utils/CustomFunctions';
+import {useTheme} from '../utils/ThemeContext';
 import {useNavigation} from '@react-navigation/native';
 
 export type BasicEmployeeProps = {
@@ -17,14 +18,28 @@ const ProfileCard = (props: BasicEmployeeProps) => {
   const photoURL: string = `${getCustomState()['company-api-url']}/employees/${
     props.id
   }/image`;
-
+  const theme = useTheme().theme === 'dark' ? '-dark' : '';
+  const customStyles = StyleSheet.create({
+    employeeCard: {
+      backgroundColor: getCustomState().custom[`background-2${theme}`],
+      flexDirection: 'row',
+      alignItems: 'center',
+      padding: 10,
+    },
+    employeeCardNameText: {
+      flex: 1,
+      fontSize: 25,
+      color: getCustomState().custom[`text-primary${theme}`],
+      justifyContent: 'center',
+    },
+  });
   return (
     <TouchableHighlight
-      style={styles.employeeCard}
+      style={styles.cardContainer}
       onPress={() => {
-        navigation.navigate('UserInfo', {id: props.id});
+        navigation.navigate('UserInfo', { id: props.id });
       }}>
-      <View style={styles.cardHeader}>
+      <View style={customStyles.employeeCard}>
         <FastImage
           source={{
             uri: photoURL,
@@ -36,7 +51,7 @@ const ProfileCard = (props: BasicEmployeeProps) => {
           }}
           style={styles.employeeCardImage}
         />
-        <Text style={styles.employeeCardNameText}>
+        <Text style={customStyles.employeeCardNameText}>
           {props.name} {props.surname}
         </Text>
       </View>
@@ -47,30 +62,16 @@ const ProfileCard = (props: BasicEmployeeProps) => {
 export default ProfileCard;
 
 const styles = StyleSheet.create({
-  employeeCard: {
-    backgroundColor: 'grey',
-    marginBottom: 6,
-  },
-  cardHeader: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    padding: 10,
+  cardContainer: {
+    marginTop: 20,
+    borderRadius: 10,
+    overflow: 'hidden',
+    marginHorizontal: 10,
   },
   employeeCardImage: {
     width: 60,
     height: 60,
     marginRight: 10,
     borderRadius: 50,
-  },
-  employeeCardNameText: {
-    flex: 1,
-    fontSize: 25,
-    color: 'black',
-    justifyContent: 'center',
-  },
-  cardContent: {
-    flexDirection: 'row',
-    justifyContent: 'center',
-    padding: 10,
   },
 });
