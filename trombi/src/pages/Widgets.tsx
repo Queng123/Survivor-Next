b7c23a982af4cec37d3a6061310dd20ccf77ecb2
Eliot Baratte <eliot.baratte@epitech.eu>
2023-09-15 03:22:31 +0200

    feat(add-widget-page): make the page customizable via the database

--- trombi/src/pages/Widgets.tsx
+++ trombi/src/pages/Widgets.tsx
@@ -2,19 +2,39 @@ import React from 'react';
 import {View, Pressable, StyleSheet, Text} from 'react-native';
 import {WidgetContainer} from '../components/WidgetContainer';
 import {useNavigation} from '@react-navigation/native';
+import {useTheme} from '../utils/ThemeContext';
+import {getCustomState} from '../utils/CustomFunctions';
 
 const Widgets = () => {
   const navigation = useNavigation();
+  const theme = useTheme().theme === 'dark' ? '-dark' : '';
 
+  const customStyles = StyleSheet.create({
+    button: {
+      alignItems: 'center',
+      justifyContent: 'center',
+      width: 60,
+      height: 60,
+      borderRadius: 30,
+      backgroundColor: getCustomState().custom[`button-primary${theme}`],
+      position: 'absolute',
+      bottom: 20,
+      right: 20,
+    },
+    text: {
+      fontSize: 30,
+      color: getCustomState().custom[`text-primary${theme}`],
+    },
+  });
   return (
     <View style={styles.view}>
       <WidgetContainer />
       <Pressable
-        style={styles.button}
+        style={customStyles.button}
         onPress={() => {
           navigation.navigate('WidgetSelector');
         }}>
-        <Text style={styles.text}>+</Text>
+        <Text style={customStyles.text}>+</Text>
       </Pressable>
     </View>
   );
@@ -26,20 +46,5 @@ const styles = StyleSheet.create({
   view: {
     width: '100%',
     height: '100%',
-  },
-  button: {
-    alignItems: 'center',
-    justifyContent: 'center',
-    width: 60,
-    height: 60,
-    borderRadius: 30,
-    backgroundColor: 'blue',
-    position: 'absolute',
-    bottom: 20,
-    right: 20,
-  },
-  text: {
-    fontSize: 30,
-    color: 'white',
   },
 });
